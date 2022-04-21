55ce48390d121ad4e72cb5fede4cce85c6413f82
adrianomonteiroweb <adrianomonteiroweb@gmail.com>
2022-04-21 09:21:10 -0300

    feat: req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    try:
+        counting = 0
+        
+        for period in permanence_period:
+            if period[0] <= target_time <= period[1]:
+                counting += 1
+
+        return counting
+    except TypeError:
+        return None            
