5377648479e1ce574567d1f991b3699682b9090b
adrianomonteiroweb <adrianomonteiroweb@gmail.com>
2022-04-21 09:35:05 -0300

    fix: Linter correction req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,11 +1,11 @@
 def study_schedule(permanence_period, target_time):
     try:
         counting = 0
-        
+
         for period in permanence_period:
             if period[0] <= target_time <= period[1]:
                 counting += 1
 
         return counting
     except TypeError:
-        return None            
+        return None
